68975c4b4edc8fd1df166127301401c6399c0a95
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-07 19:09:49 -0300

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhoast/javascriptNote', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,  
+}).then(()=> console.log('Connection succesful'))
+  .catch((err) => console.log(err));
