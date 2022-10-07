61c9a0535b85af4636f39cd33efd9f5427236b6a
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-07 10:34:18 -0300

    Delete database.js

--- production/javascript_note_api/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhoast/javascriptNote', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex:true
-}).then(()=> console.log('Connection succesful'))
-  .catch((err) => console.log(err));
