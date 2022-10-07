0a3755eeeca27bef32e86c8ead1940cde96c8cc2
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-07 19:19:51 -0300

    Update note.js

--- app/models/note.js
+++ app/models/note.js
@@ -4,5 +4,12 @@ let noteSchema = new mongoose.Schema({
   titlte: String,
   body: String,
   create_at: { type: Date, default: Date.now },
-  update_at: { type: Date, default: Date.now }
+  update_at: { type: Date, default: Date.now },
+  author: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 })
+
+module.exports = mongoose.model('Note' , noteSchema);
