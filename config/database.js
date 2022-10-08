308ce186a3beb85728081b55aa1a70bf743f08a3
Marcus Vinicius Abdalla <93200321+MarcusViniciusAbdalla@users.noreply.github.com>
2022-10-07 21:23:33 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhoast/javascriptNote', {
+mongoose.connect('mongodb://localhoast:27017', {
   useNewUrlParser: true,
   useUnifiedTopology: true,  
 }).then(()=> console.log('Connection succesful'))
   .catch((err) => console.log(err));
+
+  /*const mongoose = require('mongoose')
+
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
+async function main() {
+  await mongoose.connect(MONGO_URL)
+  console.log('Conectou ao Mongoose!')
+}
+
+main().catch((err) => console.log(err))
+
+module.exports = mongoose*/
